fe2ced237ea37c4f69ee2ea36130f3c08d9429f1
LILKEK361 <nickhillmann74@gmail.com>
2022-11-14 18:50:04 +0100

    WTF

---
+++ Kniffel_server/src/ReadThread.java
@@ -0,0 +1,5 @@
+public class ReadThread extends Thread {
+
+    
+
+}
--- Kniffel_server/src/gamedb/GameDB.java
+++ Kniffel_server/src/gamedb/GameDB.java
@@ -210,7 +210,7 @@ public class GameDB {
 
         for(DataConnectedUser n : connectedUserList)
         {
-            
+            n.send
         }
 
 
